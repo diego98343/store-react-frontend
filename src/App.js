a29ec19d46f6dd21447f4952a27013a0f257a190
diegocolonia <diegocolonia98@yahoo.com>
2024-02-07 23:13:19 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,60 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import { Navbar, Sidebar, Footer } from './components'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Sidebar, Footer } from "./components";
+import {Home,Products,SingleProduct,About,Cart,Error,Checkout,Private} from './pages';
+import styled from "styled-components";
+
+// const Button = styled.button`
+//   background: green;
+//   color: white;
+// `;
+
+// const Container = styled.div`
+// background:red;
+// color:white;
+// .hero{
+//   font-size: 8rem;
+// }
+// `
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <Router>
+      <Navbar/>
+      <Sidebar/>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+
+        <Route exact path="/product">
+          <Products/>
+        </Route>
+
+        <Route exact path="/products/id" children={<SingleProduct/>}>
+          <SingleProduct/>
+        </Route>
+
+        <Route exact path="/About">
+          <About/>
+        </Route>
+
+        <Route exact path="/cart">
+          <Cart/>
+        </Route>
+
+        <Route exact path="/checkout">
+          <Checkout/>
+        </Route>
+
+        <Route path="*">
+          <Error/>
+        </Route>
+
+      </Switch>
+      <Footer/>
+    </Router>
+     )
 }
 
-export default App
+export default App;
